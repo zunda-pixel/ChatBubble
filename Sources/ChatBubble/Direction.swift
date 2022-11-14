28e1928e0d7dc534ecc53de0a8911de25e44d168
zunda <zunda.dev@gmail.com>
2022-11-14 23:59:54 +0900

    Add Direction

---
+++ Sources/ChatBubble/Direction.swift
@@ -0,0 +1,25 @@
+//
+//  Direction.swift
+//
+
+extension ChatBubble {
+  public enum Direction {
+    case trailingTop
+    case trailingBottom
+    case leadingTop
+    case leadingBottom
+    
+    var isLeading: Bool {
+      switch self {
+      case .trailingTop:
+        return false
+      case .trailingBottom:
+        return false
+      case .leadingTop:
+        return true
+      case .leadingBottom:
+        return true
+      }
+    }
+  }
+}
