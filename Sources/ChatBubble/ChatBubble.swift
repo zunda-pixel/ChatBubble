d5e3a5100590818bad974fca0abfb2b08f05f23e
zunda <zunda.dev@gmail.com>
2022-11-15 00:01:18 +0900

    add Preview

--- Sources/ChatBubble/ChatBubble.swift
+++ Sources/ChatBubble/ChatBubble.swift
@@ -1,15 +1,17 @@
 import SwiftUI
 
-struct ChatBubble: Shape {
+public struct ChatBubble: Shape {
   var cornerRadius: Double
   
-  func path(in rect: CGRect) -> Path {
+  public init(cornerRadius: Double) {
+    self.cornerRadius = cornerRadius
+  }
+  
+  public func path(in rect: CGRect) -> Path {
     Path { path in
-      let tailSize = CGSize(
-        width: cornerRadius / 2,
-        height: cornerRadius / 2
-      )
+      let tailSize = cornerRadius / 2
       
+      // 左上角丸
       path.addArc(
         center: CGPoint(
           x: rect.minX + cornerRadius,
@@ -36,7 +38,7 @@ struct ChatBubble: Shape {
       // しっぽ上部
       path.addQuadCurve(
         to: CGPoint(
-          x: rect.maxX + tailSize.width,
+          x: rect.maxX + tailSize,
           y: rect.minY
         ),
         control: CGPoint(
@@ -49,11 +51,11 @@ struct ChatBubble: Shape {
       path.addQuadCurve(
         to: CGPoint(
           x: rect.maxX,
-          y: rect.minY + (cornerRadius / 2) + tailSize.height
+          y: rect.minY + tailSize * 2
         ),
         control: CGPoint(
           x: rect.maxX,
-          y: rect.minY + tailSize.height
+          y: rect.minY + tailSize
         )
       )
       
@@ -85,13 +87,82 @@ struct ChatBubble: Shape {
 }
 
 
-struct ChatBubble_Preview: PreviewProvider {
-  static var previews: some View {
-    Text("Hello")
+
+extension View {
+  public func chatBubble(direction: ChatBubble.Direction, cornerRadius: Double, color: Color) -> some View {
+    self
       .padding()
       .background {
-        ChatBubble(cornerRadius: 17)
-          .foregroundColor(.red.opacity(0.5))
+        ChatBubble(cornerRadius: cornerRadius)
+          .rotateChatBubble(direction: direction)
+          .foregroundColor(color)
       }
+      .padding(direction.isLeading ? .leading : .trailing, cornerRadius / 2)
+  }
+  
+  func rotateChatBubble(direction: ChatBubble.Direction) -> some View {
+    switch direction {
+    case .trailingTop:
+      return self.rotation3DEffect(.init(degrees: 180), axis: (0, 0, 0))
+    case .trailingBottom:
+      return self.rotation3DEffect(.init(degrees: 180), axis: (1, 0, 0))
+    case .leadingTop:
+      return self.rotation3DEffect(.init(degrees: 180), axis: (0, 1, 0))
+    case .leadingBottom:
+      return self.rotation3DEffect(.init(degrees: 180), axis: (0, 0, 1))
+    }
+  }
+}
+
+
+struct ChatBubble_Preview: PreviewProvider {
+  struct Chat: Identifiable {
+    let id = UUID()
+    let text: String
+    let direction: ChatBubble.Direction
+  }
+  
+  static let chats: [Chat] = [
+    .init(text: "In order to meet the deadline for delivery.", direction: .trailingTop),
+    .init(text: "The business results are above average.", direction: .leadingTop),
+    .init(text: "Handle phone calls.", direction: .leadingTop),
+    .init(text: "Notification of rescheduling of next week’s regular meeting.", direction: .trailingTop),
+    .init(text: "Request a customer to introduce other customers.", direction: .trailingTop),
+  ]
+  
+  static var previews: some View {
+    VStack {
+      ForEach(chats) { chat in
+        HStack(alignment: .top) {
+          if !chat.direction.isLeading {
+            Spacer()
+          }
+          
+          Text(chat.text)
+            .chatBubble(
+              direction: chat.direction,
+              cornerRadius: 17,
+              color: .blue.opacity(0.5)
+            )
+            .frame(maxWidth: 250, alignment: chat.direction.isLeading ? .leading : .trailing)
+            //.border(.red)
+          
+          if chat.direction.isLeading {
+            Spacer()
+          }
+        }
+      }
+    }
+  }
+}
+
+struct SingleChatBubble: PreviewProvider {
+  static var previews: some View {
+    Text("Request a customer to introduce other customers. Request a customer to introduce other customers")
+      .chatBubble(
+        direction: .leadingTop,
+        cornerRadius: 17,
+        color: .blue.opacity(0.5)
+      )
   }
 }
