efe23e9d8b9816d9b23c8e7b262f3874e7c1a189
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-06-05 01:00:45 +0900

    add watchOS

--- Sources/ChatBubble/ChatBubble.swift
+++ Sources/ChatBubble/ChatBubble.swift
@@ -178,7 +178,7 @@ extension View {
   }
 }
 
-@available(iOS 16.0, macOS 13.0, tvOS 16.0, *)
+@available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
 #Preview {
   struct Chat: Identifiable {
     let id = UUID()
