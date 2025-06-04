3ed4a3d249866d40acf6c32f26915197d2b84bd8
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-06-04 19:25:53 +0900

    update swift version to 6

--- Package.swift
+++ Package.swift
@@ -1,11 +1,17 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "ChatBubble",
-  platforms: [.iOS(.v15), .macOS(.v12), .tvOS(.v15), .watchOS(.v8), .macCatalyst(.v15)],
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v12),
+    .tvOS(.v15),
+    .watchOS(.v8),
+    .macCatalyst(.v15)
+  ],
   products: [
     .library(
       name: "ChatBubble",
